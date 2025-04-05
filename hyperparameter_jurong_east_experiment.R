84dc4f1cfdd25565dd9da6b0215635eda59bec31
Ryhana Aminnuddin Rasidi <E0408813@u.nus.edu>
2025-04-05 19:18:37 +0800

    Update hyperparameter_jurong_east_experiment.R

--- hyperparameter_jurong_east_experiment.R
+++ hyperparameter_jurong_east_experiment.R
@@ -154,7 +154,7 @@ get_bimonth_year_columns <- function(df){
 
 
 #Original Data used:
-dir <- "C:/Users/samue/Documents/Masters/Sem 2/ST5188/st5188/Data/Final/final_JURONG EAST_long.csv"
+dir <- "ST5188/Data/Final/final_JURONG EAST_long.csv"
 data <- read.csv(dir)
 #Define Bounding Box to reduce computational complexity 
 resolution <- get_resolution(data)
